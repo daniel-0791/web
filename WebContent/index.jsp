6c81eff6fc3402bc65c4878166b1a3f6cf5dda21
daniel <15170231053@163.com>
2018-12-02 16:51:02 +0800

    Initial commit

---
+++ WebContent/index.jsp
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>launch</title>
+</head>
+<body>
+	<form action="MyServlet" method="post">
+		account:<input type="text" name="account"><br>
+		password:<input type="text" name="password"><br>
+		<input type="submit" value="submit">
+	</form>
+</body>
+</html>
---
+++ src/com/servlet/MyServlet.java
@@ -0,0 +1,45 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String account=request.getParameter("account");
+		String password=request.getParameter("password");
+		PrintWriter out=response.getWriter();
+		out.println("account="+account+";password="+password);
+		System.out.println("account="+account);
+		System.out.println("password="+password);
+	}
+
+}
